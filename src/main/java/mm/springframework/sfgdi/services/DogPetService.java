612eef36ac2d2db9856d7de97b19b50bfcdcde1a
Michele Mercaldi <michele.mercaldi@gmail.com>
2024-01-11 14:39:55 +0100

    services back in service package

--- src/main/java/mm/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/mm/springframework/sfgdi/services/DogPetService.java
@@ -1,4 +1,4 @@
-package mm.springframework.pets;
+package mm.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
--- src/main/java/mm/springframework/sfgdi/services/PetService.java
+++ src/main/java/mm/springframework/sfgdi/services/PetService.java
@@ -1,4 +1,4 @@
-package mm.springframework.pets;
+package mm.springframework.sfgdi.services;
 
 /**
  * Created by jt on 12/28/19.
